db4c37a41f4c8656d667f779ae5b3e5333b68c7e
Erle Erlandsen Sandø <ees@norsktest.no>
2025-02-27 09:29:19 +0100

    lagt inn del av design for ny kalkulator

---
+++ src/lib/Calculator.svelte
@@ -0,0 +1,102 @@
+<script>
+	let displayed = $state("");
+	let equalstate = $state(0);
+	let buttons = ["CE","bs", "sq", "&divide;", "7","8","9","x","4","5","6","-","1","2","3","+","0",".","="]
+</script>
+
+<div class="box outer">
+    <input class="box inputbox" style="color:black">	
+    
+    <div class="buttoncontainer">
+        <!-- Rader 3 til 7 med 4 kolonner -->
+        <button class="operator">C</button>
+        <button class="operator">&;</button>
+        <button class="operator">&radic;</button>
+        <button class="operator">&divide;</button>
+
+        <button class="number">7</button>
+        <button class="number">8</button>
+        <button class="number">9</button>
+        <button class="operator">&times;</button>
+        
+        <button class="number">4</button>
+        <button class="number">5</button>
+        <button class="number">6</button>
+        <button class="operator">-</button>
+        
+        <button class="number">1</button>
+        <button class="number">2</button>
+        <button class="number">3</button>
+        <button class="operator">+</button>
+        
+        <button class="number zero">0</button>
+        <button class="number">,</button>
+        <button class="equal">=</button>        
+    </div>
+
+</div>
+<style>
+    .buttoncontainer {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(5, 1fr);
+        gap: 5px;
+        margin-left: 15px;
+    }
+	.box {
+		border: 2px solid black;
+		border-radius: 0.5em;
+	}
+
+    .operator{
+        font-size:20px;
+        background:lightgray;
+    }
+
+    .number{
+        font-size:20px;
+    }
+
+    .zero {
+        grid-column: span 2;
+    }
+
+    .equal {
+        background: orange;
+    }    
+
+	button {
+		width:50px;
+		height:50px;
+		border: 1px solid black;
+		position: relative;
+		top: 20px;
+		border-radius: 6px
+	}
+
+
+	.outer {
+		width: 250px;
+		height: 380px;
+		background-color: darkgray;
+		text-align: center;
+		margin: 100px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: auto auto repeat(5, 1fr);
+        gap: 3px
+	}
+
+	.inputbox {
+		width: 225px;
+		height: 40px;
+		margin-top: 15px;
+        margin-left: 5px;
+		font-size:25px;
+		background-color: white;
+		text-align: right;
+		padding: 5px 5px;
+		position: relative;
+        grid-column: span 4;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Calculator from "$lib/Calculator.svelte";
     import CalculatorErle from "$lib/Calculator_Erle.svelte";
     import CalculatorTerje from "$lib/Calculator_Terje.svelte";
 
@@ -6,3 +7,4 @@
 
 <CalculatorErle />
 <CalculatorTerje />
+<Calculator />
