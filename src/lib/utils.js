e20dcf9c4ba450f68c6c3cf5d3f18991f47fd5f3
Erle Erlandsen Sandø <ees@norsktest.no>
2025-03-17 12:39:54 +0100

    test failed on is_operator

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,13 +3,7 @@ const operatorsigns = "+-*//×/g/÷/g"
 const numbers = "0123456789"
 
 export function is_operator(value) {
-    if (value=""){
-        return false;
-    } else if (operatorsigns.includes(value)){
-        return true;
-    } else {
-        return false;
-    }
+    return (value = "" ? false : operatorsigns.includes(value))
 }
 export const is_number = (value) => numbers.includes(value)
 export const lastchar = (value) => value[value.length-1]
